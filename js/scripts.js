c83c23b2a98f9e0222954988ea649b8a4eac5c92
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-10-27 15:22:42 -0500

    add Crossout item (jQuery)

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,23 @@ function newItem(){
 //javascript
 //1. Adding a new item to the list of items:
 
-   let inputValue = $('#input').val();
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-   if (inputValue === '') {
+  if (inputValue === '') {
      alert("You must write something!");
-   }
-   else {
-     $('#list').append(`<li>${inputValue}</li>`);
-   }
+  }
+  else {
+     $('#list').append(li);
+  }
 
  //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
+  function crossOut() {
+ 	  li.addClass("strike");
  	}
 
- 	li.addEventListener("dblclick",crossOut);
+  li.on("dblclick", crossOut);
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
