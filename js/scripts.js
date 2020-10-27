815b12f44bc378cd454f41b60ca7a8fce4843ccd
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-10-27 14:44:28 -0500

    add New item (jQuery)

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,14 @@ function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+
+   let inputValue = $('#input').val();
 
    if (inputValue === '') {
      alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
+   }
+   else {
+     $('#list').append(`<li>${inputValue}</li>`);
    }
 
  //2. Crossing out an item from the list of items:
